1e888f0289d2eafde44c6592b0fdc3328a838ace
Sp1r1tual <andriy0383@gmail.com>
2025-02-25 20:01:32 +0200

    Added theme change. Show quote and author separately. Minor visual improvements

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 const quotes = [
-    "Роби, що можеш, з тим, що маєш, там, де ти є. — Теодор Рузвельт",
-    "Єдиний спосіб робити велику справу – любити те, що ти робиш. — Стів Джобс",
-    "Не бійся провалу. Це не кінець світу. — Дензел Вашингтон",
-    "Життя – це 10% того, що з тобою відбувається, і 90% того, як ти на це реагуєш. — Чарльз Свіндолл",
+    {
+        quote: "Роби, що можеш, з тим, що маєш, там, де ти є",
+        author: "Теодор Рузвельт",
+    },
+    {
+        quote: "Єдиний спосіб робити велику справу – любити те, що ти робиш",
+        author: "Стів Джобс",
+    },
+    {
+        quote: "Не бійся провалу. Це не кінець світу",
+        author: "Дензел Вашингтон",
+    },
+    {
+        quote: "Життя – це 10% того, що з тобою відбувається, і 90% того, як ти на це реагуєш",
+        author: "Чарльз Свіндолл",
+    },
 ];
 
 let lastIndex;
 
 function generateQuote() {
     let randomIndex;
+
     do {
         randomIndex = Math.floor(Math.random() * quotes.length);
     } while (randomIndex === lastIndex);
 
     lastIndex = randomIndex;
-    document.getElementById("quote").innerText = quotes[randomIndex];
+
+    document.getElementById("quote").innerHTML = quotes[randomIndex].quote;
+
+    document.getElementById("author").innerText =
+        "— " + quotes[randomIndex].author;
 }
+
+document.getElementById("theme-toggle").addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
