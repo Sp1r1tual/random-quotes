b6e01ae55275f7236e4358a86872c5a1b8f3e2ec
Sp1r1tual <andriy0383@gmail.com>
2025-02-26 23:57:53 +0200

    Show favorite quotes

--- main.js
+++ main.js
@@ -1,26 +1,66 @@
 import quotes from "./quotes.js";
 
-let lastIndex;
+const toggleFavoriteBtn = document.getElementById("toggle-favorite");
+const favoritesContainer = document.getElementById("favorites-container");
+let lastIndex = -1;
+let currentQuoteIndex;
 
-function generateQuote() {
-    let randomIndex;
-
-    do {
-        randomIndex = Math.floor(Math.random() * quotes.length);
-    } while (randomIndex === lastIndex);
-
-    lastIndex = randomIndex;
-
-    document.getElementById("quote").innerHTML = quotes[randomIndex].quote;
-
-    document.getElementById("author").innerText =
-        "— " + quotes[randomIndex].author;
+function handleButtonClick(buttonId, action) {
+    document.getElementById(buttonId).addEventListener("click", action);
 }
 
-document
-    .getElementById("quote-button")
-    .addEventListener("click", generateQuote);
+function generateQuote() {
+    do {
+        currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+    } while (currentQuoteIndex === lastIndex);
+
+    const randomQuote = quotes[currentQuoteIndex];
+    lastIndex = currentQuoteIndex;
+
+    document.getElementById("quote").innerHTML =
+        quotes[currentQuoteIndex].quote;
+    document.getElementById("author").innerText =
+        "— " + quotes[currentQuoteIndex].author;
+
+    toggleFavoriteBtn.textContent = randomQuote.isFavorite
+        ? "Вилучити"
+        : "Улюблене";
+
+    toggleFavoriteBtn.style.display = "inline-block";
+}
+
+handleButtonClick("quote-button", generateQuote);
+
+function toggleFavorite() {
+    const currentQuote = quotes[currentQuoteIndex];
+
+    currentQuote.isFavorite = !currentQuote.isFavorite;
+    toggleFavoriteBtn.textContent = currentQuote.isFavorite
+        ? "Вилучити"
+        : "Улюблене";
+
+    if (currentQuote.isFavorite) {
+        const favoriteCard = document.createElement("div");
+
+        favoriteCard.classList.add("favorite-card");
+        favoriteCard.innerHTML = `<p>${currentQuote.quote} – ${currentQuote.author}</p>`;
+        favoritesContainer.appendChild(favoriteCard);
+    } else {
+        const favoriteCards = document.querySelectorAll(".favorite-card");
+
+        favoriteCards.forEach((card) => {
+            if (card.textContent.includes(currentQuote.quote)) {
+                card.remove();
+            }
+        });
+    }
+}
+
+handleButtonClick("toggle-favorite", toggleFavorite);
 
 document.getElementById("theme-toggle").addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
+    document
+        .getElementById("favorites-container")
+        .classList.toggle("dark-mode");
 });
